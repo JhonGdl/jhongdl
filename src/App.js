7c6d01228bcebe4bbfa6fed6d220883e3998ee56
Jonathan Sánchez Ramírez <jhongdl@gmail.com>
2020-11-02 22:26:36 -0600

    2020-11-02

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="intro">Hello, I'm Dinesh!</div>
+      <div className="tagline">All-Star Dev | Code Fanatic | Linux Hacker | Bleh</div>
+      <div className="icons-social">
+        <a target="_blank" href="https://github.com/flexdinesh"><i className="fab fa-github" /></a>
+        <a target="_blank" href="https://twitter.com/flexdinesh"><i className="fab fa-twitter" /></a>
+        <a target="_blank" href="https://dev.to/flexdinesh"><i className="fab fa-dev" /></a>
+        <a target="_blank" href="https://stackoverflow.com/story/flexdinesh"><i className="fab fa-stack-overflow" /></a>
+        <a target="_blank" href="https://www.linkedin.com/in/dineshpandiyan"><i className="fab fa-linkedin" /></a>
+        <a target="_blank" href="https://medium.com/@flexdinesh"><i className="fab fa-medium" /></a>
+        <a target="_blank" href="https://www.freecodecamp.org"><i className="fab fa-free-code-camp" /></a>
+        <a target="_blank" href="https://www.behance.net"><i className="fab fa-behance" /></a>
+        <a target="_blank" href="https://codepen.io"><i className="fab fa-codepen" /></a>
+      </div>
+    </main>
   );
 }
 
